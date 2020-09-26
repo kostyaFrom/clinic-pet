e7f6c801497e9c012de7b9911033f93417777b00
@kostyaFrom <korustlt@gmail.com>
2020-09-26 20:08:34 +0400

    Enhanced Owners with contact info and Pets on startup. Close #33

--- pet-clinic-data/src/main/java/com/korustlt/petclinic/sevices/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/korustlt/petclinic/sevices/map/OwnerServiceMap.java
@@ -1,13 +1,25 @@
 package com.korustlt.petclinic.sevices.map;
 
 import com.korustlt.petclinic.model.Owner;
+import com.korustlt.petclinic.model.Pet;
 import com.korustlt.petclinic.sevices.OwnerService;
+import com.korustlt.petclinic.sevices.PetService;
+import com.korustlt.petclinic.sevices.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -20,7 +32,27 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner type) {
-        return super.save(type);
+        if (type != null) {
+            if (type.getPets() != null) {
+                type.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+
+                    } else {
+                        throw new RuntimeException("Pet type is required");
+                    }
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(type);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/korustlt/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/korustlt/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.korustlt.petclinic.bootstrap;
 
 import com.korustlt.petclinic.model.Owner;
+import com.korustlt.petclinic.model.Pet;
 import com.korustlt.petclinic.model.PetType;
 import com.korustlt.petclinic.model.Vet;
 import com.korustlt.petclinic.sevices.OwnerService;
@@ -8,6 +9,8 @@ import com.korustlt.petclinic.sevices.PetTypeService;
 import com.korustlt.petclinic.sevices.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,11 +39,29 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Grove Street");
+        owner1.setCity("LA");
+        owner1.setTelephone("893723569645");
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glean");
+        owner2.setAddress("123 Avenue Street");
+        owner2.setCity("NY");
+        owner2.setTelephone("893758946500");
+        Pet fionaPet = new Pet();
+        fionaPet.setPetType(savedCatPetType);
+        fionaPet.setOwner(owner2);
+        fionaPet.setBirthDate(LocalDate.now());
+        fionaPet.setName("Margo");
+        owner2.getPets().add(fionaPet);
         ownerService.save(owner2);
 
         System.out.println("Loaded owners...");
