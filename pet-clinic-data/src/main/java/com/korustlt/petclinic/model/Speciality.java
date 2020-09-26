f3a6af80314c7eeaffa63a2ff314899aa1eff4a3
@kostyaFrom <korustlt@gmail.com>
2020-09-26 16:28:17 +0400

    Created Vet Speciality Entity associated to Vet. Close #27

---
+++ pet-clinic-data/src/main/java/com/korustlt/petclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package com.korustlt.petclinic.model;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
