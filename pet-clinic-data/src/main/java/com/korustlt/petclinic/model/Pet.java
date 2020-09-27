8cf12f9f56cee3b8e8a3073483e3005ab3ef21a4
@kostyaFrom <korustlt@gmail.com>
2020-09-27 21:28:53 +0400

    Converted Owners to JPA Entities. Close #36

--- pet-clinic-data/src/main/java/com/korustlt/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/korustlt/petclinic/model/Pet.java
@@ -1,12 +1,25 @@
 package com.korustlt.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "pet_name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- pet-clinic-data/src/main/java/com/korustlt/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/korustlt/petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package com.korustlt.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "type_name")
     private String name;
 
     public String getName() {
