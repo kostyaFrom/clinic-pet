8fa5c9b2ff9d5a8624d4f7aebcac9f503e4dea94
@kostyaFrom <korustlt@gmail.com>
2020-09-30 22:20:01 +0400

    Created Visit map service. Close #46

---
+++ pet-clinic-data/src/main/java/com/korustlt/petclinic/sevices/VisitService.java
@@ -0,0 +1,6 @@
+package com.korustlt.petclinic.sevices;
+
+import com.korustlt.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/korustlt/petclinic/sevices/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package com.korustlt.petclinic.sevices.map;
+
+import com.korustlt.petclinic.model.Visit;
+import com.korustlt.petclinic.sevices.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit type) {
+        super.delete(type);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null ||
+                visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
